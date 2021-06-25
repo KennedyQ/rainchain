dcec3b66822815b4ca3dab968ce4326fcfa50130
KennedyQ <80281153+KennedyQ@users.noreply.github.com>
2021-06-25 05:17:00 -0400

    Added support for basic crypto-currency Droplets

--- RainChain.py
+++ RainChain.py
@@ -1,19 +1,32 @@
-from datetime import datetime
-from datetime import timezone
+from datetime import datetime, timezone
 import hashlib
 import json
+from secp256k1 import secp256k1
+import secrets
 
 class Block():
 
-	def __init__(self, data, previoushash):
+	def __init__(self, previoushash):
 		self.__previoushash = previoushash
-		self.__data = data
 		self.__timestamp = str(int((datetime.now(timezone.utc) - datetime(1970, 1, 1, 0, 0, 0, 0, timezone.utc)).total_seconds() * 1000))
 		self.__nonce = 0
+		self.__merkleroot = ""
 		self.__hash = self.calculateHash()
+		self.__transactions = []
+
+	def addTransaction(self, transaction, chain = None):
+		if transaction is None:
+			return False
+		if not self.__previoushash == "0":
+			if not transaction.processTransaction(chain):
+				print("The Transaction Failed to Process and was Discarded.")
+				return False
+		self.__transactions.append(transaction)
+		print("Transaction Successfully Added to the Block")
+		return True
 
 	def calculateHash(self):
-		hash = Cryptography().applySha256(Cryptography().encodeUnicode(self.__previoushash + self.__timestamp + str(self.__nonce) + self.__data))
+		hash = Cryptography().applySha256(Cryptography().encodeUnicode(self.__previoushash + self.__timestamp + str(self.__nonce) + self.__merkleroot))
 		return hash
 
 	def getHash(self):
@@ -23,6 +36,7 @@ class Block():
 		return self.__previoushash
 
 	def mineBlock(self, difficulty):
+		merkleroot = Cryptography().getPseudoMerkleRoot(self.__transactions)
 		target = ""
 		for i in range(0, difficulty):
 			target += "0"
@@ -39,21 +53,85 @@ class Cryptography():
 	def applySha256(self, string):
 		return hashlib.sha256(string).hexdigest()
 
-	def encodeUnicode(self, string):
-		return string.encode('utf-8')
-	
+	def ECDSASign(self, key, data):
+		nvalue = secp256k1().getCurve().field.n
+		messagehash = self.applySha256(self.encodeUnicode(data))
+		kvalue = secrets.randbelow(nvalue-1) + 1
+		rvalue = (kvalue * secp256k1().getCurve().g).x
+		kinverse = self.modularInverse(kvalue, nvalue)
+		svalue = kinverse * (int(messagehash, 16) + rvalue * key) % nvalue
+		return [rvalue, svalue]
+
+	def ECDSAVerify(self, key, data, signature):
+		nvalue = secp256k1().getCurve().field.n
+		messagehash = self.applySha256(self.encodeUnicode(data))
+		svalue = self.modularInverse(signature[1], nvalue)
+		r1 = (int(messagehash, 16) * svalue % nvalue) * secp256k1().getCurve().g
+		r2 = (signature[0] * svalue % nvalue) * key
+		rvalue = (r1 + r2).x % nvalue
+		if rvalue == signature[0]:
+			return True
+		else:
+			return False
+
 	def decodeUnicode(self, string):
 		return string.decode('utf-8')
 
+	def encodePublicKey(self, key):
+		return '0' + str(2 + key.y % 2) + str(hex(key.x)[2:])		
+
+	def encodeUnicode(self, string):
+		return string.encode('utf-8')
+	
+	def getPseudoMerkleRoot(self, transactions):
+		count = len(transactions)
+		previoustreelayer = []
+		treelayer = []
+		for i in transactions:
+			previoustreelayer.append(i.getHash())
+		while count > 1:
+			for i in range(1,len(previoustreelayer)):
+				treelayer.append(self.applySha256(previousTreeLayer[i-1] + previousTreeLayer[i]))
+			count = len(treelayer)
+			previoustreelayer = [i for i in treelayer]
+		if len(treelayer) == 1:
+			merkleroot = treelayer[0]
+		else:
+			merkleroot = ""
+		return merkleroot
+
+	def modularInverse(self, base, modulo):
+		if base < 0 or modulo <= base:
+			base = base % modulo
+		currentbase = base
+		currentmodulo = modulo
+		baseconstantu, baseconstantv, moduloconstantu, moduloconstantv = 1, 0, 0, 1
+		while currentbase != 0:
+			quotient, currentbase, currentmodulo = divmod(currentmodulo, currentbase) + (currentbase,)
+			baseconstantu, baseconstantv, moduloconstantu, moduloconstantv = moduloconstantu - quotient*baseconstantu, moduloconstantv - quotient*baseconstantv, baseconstantu, baseconstantv
+		if not currentmodulo == 1:
+			return None
+		if moduloconstantu > 0:
+			return moduloconstantu
+		else:
+			return moduloconstantu + modulo
+
 class RainChain():
 
-	def __init__(self, initialData):
+	def __init__(self, genesisblock, initialutxo):
 		self.__difficulty = 5
-		self.__firstblock = Block(initialData, "0")
+		self.__firstblock = genesisblock
 		self.__blockchain = [self.__firstblock]
 		print("Trying to mine first block:")
 		self.__firstblock.mineBlock(self.__difficulty)
 		self.__length = 1
+		self.__UTXOs = {initialutxo.getId(): initialutxo}
+
+	def addUTXO(self, id, output):
+		self.__UTXOs[id] = output
+
+	def getAllUTXOs(self):
+		return self.__UTXOs
 
 	def getBlock(self, index):
 		try:
@@ -61,12 +139,17 @@ class RainChain():
 		except IndexOutOfBounds:
 			print("Index out of bounds")
 
-	def insertBlock(self, data):
-		newblock = Block(data, self.__blockchain[self.__length - 1].getHash())
-		self.__blockchain.append(newblock)
-		self.__length += 1
+	def getMinimumTransaction(self):
+		return 1
+
+	def getUTXO(self, id):
+		return self.__UTXOs[id]
+
+	def insertBlock(self, block):
 		print("Trying to mine block " + str(self.__length) + ":")
-		newblock.mineBlock(self.__difficulty)
+		block.mineBlock(self.__difficulty)
+		self.__blockchain.append(block)
+		self.__length += 1
 
 	def isChainValid(self):
 		minetarget = ""
@@ -87,6 +170,9 @@ class RainChain():
 				print("Block not mined")
 				return False
 		return True
+
+	def removeUTXO(self, id):
+		del self.__UTXOs[id]
 
 	def __str__(self):
 		blockchainjson = "{\n"
